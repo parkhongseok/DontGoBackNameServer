9e2fe2b097ce4d4bd4792b6fab8731ebd2a97636
parkhongseok <123qkrghdtjr@naver.com>
2025-07-31 18:26:15 +0900

    init: project setup

---
+++ name-server/src/main/java/com/dontgoback/name_server/NameServerApplication.java
@@ -0,0 +1,13 @@
+package com.dontgoback.name_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameServerApplication.class, args);
+	}
+
+}
